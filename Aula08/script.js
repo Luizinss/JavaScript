1827a5b32c4247b52697f244a6ab3f8a4b8576c2
DevLuiz <frtluizito007@gmail.com>
2025-01-01 16:42:30 -0300

    sixth commit

---
+++ Aula08/script.js
@@ -0,0 +1,12 @@
+let n1=30
+let n2=14
+
+// let res = n1 & n2 vai ser igual a 10
+
+// let res = n1 | n2
+
+// let res = n1 ^ n2
+
+// let res = n1 >> 1 deslocando o bit para direita (>>) o valor é divido enquanto o bit deslocado para esquerda (<<) o valor é dobrado
+
+console.log(res)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Aula 01 - Hello word (console.log) --
 Aula 02 - Uso do Let e mais console.log
 Aula 03 - Conceito sobre o LET e o CONST
 Aula 04 - Operadores
+Aula 05/06 - Operadores relacionais
+Aula 07 - And, or, not
+Aula 08 - Apronfundamento em operadores
