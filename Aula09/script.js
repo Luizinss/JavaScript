0b42ba479f0477d2174f7760150edffe5a8423d9
DevLuiz <frtluizito007@gmail.com>
2025-01-02 23:28:28 -0300

    Update README.md

---
+++ Aula09/script.js
@@ -0,0 +1,21 @@
+// let n=10
+
+
+// console.log(n++)
+
+
+// console.log(n)
+
+// n+=10
+
+// console.log(--n)
+
+// let n=10
+// let x=-n
+
+// console.log(x)
+
+let n1=10
+let n2=20
+
+console.log(n1 + "" + n2)
---
+++ Aula10/script.js
@@ -0,0 +1,20 @@
+// let num1 = 10
+// let num2 = 15
+// res = (num1 > num2 ? "Verdadeiro" : "Falso")
+
+// 0 = false
+// 1 = true
+
+//Teste lógico ? se true : se false
+
+// console.log(res)
+
+// if(res==0){
+    //     console.log("Par")
+    // }else{
+        //     console.log("Impar")
+        // }
+        
+        let st="A"
+        res = (st == "A" ? "Ativo" : "Inativo")
+        console.log(res)
